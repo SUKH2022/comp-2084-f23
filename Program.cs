d68c078f3c4ba8b92179bd81e8339cda689ae014
Sukhpreet Saini <sukhpreet.saini2020@gmail.com>
2023-09-27 16:37:19 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using WorldDominion.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add MySQL
+var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseMySQL(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Below is a dedicated conventional route (custom route)
+app.MapControllerRoute(
+    name: "privacy",
+    pattern: "privacy",
+    defaults: new { controller = "Home", action = "Privacy" });
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
